7c57288ff1828e958c0d470b883e6a9bdbc7a2c2
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-27 01:53:03 -0500

    Added notes over basic structs in go

---
+++ structs/basicStructs/go.mod
@@ -0,0 +1,3 @@
+module learnStructs
+
+go 1.25.0
---
+++ structs/basicStructs/structs.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// Structs are value types
+type Foo struct {
+	A int
+	b string
+}
+
+func main() {
+	f := Foo{}
+	fmt.Println(f)
+
+	g := Foo{10, "Hello"}
+	fmt.Println(g)
+
+	h := Foo{
+		b: "Goodbye",
+	}
+	fmt.Println(h)
+
+	h.A = 1000
+	fmt.Println(h.A)
+}

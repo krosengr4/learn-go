500cf799a1f49b81710e238fccd38b67cf16fef7
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-27 02:53:57 -0500

    Added comments

--- structs/struct-tags/structTags.go
+++ structs/struct-tags/structTags.go
@@ -15,8 +15,11 @@ type Person struct {
 func main() {
 	bob := `{ "name": "Bob", "age": 30}`
 	var b Person
+	// Unmarshal parses the json and stores the value in a pointed to value(&b)
 	json.Unmarshal([]byte(bob), &b)
 	fmt.Println(b)
+
+	// Marshal returns the JSON encodings of b
 	bob2, _ := json.Marshal(b)
 	fmt.Println(string(bob2))
 }

a8699eb82b394ea02dbe53f689c79b0867419aaa
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-28 07:46:53 -0500

    Created a playground demo that uses a rectangle struct to demonstrate a method

---
+++ playground/methods-and-shapes/go.mod
@@ -0,0 +1,3 @@
+module methodShapes
+
+go 1.25.0
---
+++ playground/methods-and-shapes/methodShapes.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type Rectangle struct {
+	width, height int
+}
+
+func (r Rectangle) CalculateArea() int {
+	return r.width * r.height
+}
+
+func main() {
+	rectangle1 := Rectangle{
+		width:  12,
+		height: 3,
+	}
+
+	rectangle2 := Rectangle{
+		width:  4,
+		height: 7,
+	}
+
+	fmt.Println("The area of rectangle 1 is:", rectangle1.CalculateArea())
+	fmt.Println("The area of rectangle 2 is:", rectangle2.CalculateArea())
+}

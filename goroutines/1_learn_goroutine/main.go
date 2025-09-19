e44d281edaa798471ff728248d4fea65fc2f9058
Kevin Rosengren <rosenkev4@gmail.com>
2025-09-18 20:16:43 -0500

    Added code notes on goroutines

---
+++ goroutines/1_learn_goroutine/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// ! When running this, nothing happens
+// ! This is bc the main goroutine did not know it had to wait for the other goroutine to complete
+
+func main() {
+	go runMe()
+}
+
+func runMe() {
+	fmt.Println("Hello, this is a goroutine")
+}

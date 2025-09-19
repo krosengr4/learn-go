6cad24c172443fb3312996abf19a65965aedf81c
Kevin Rosengren <rosenkev4@gmail.com>
2025-09-18 20:19:58 -0500

    Added notes on goroutines

---
+++ goroutines/2_learn_goroutines/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func runMe() {
+	fmt.Println("Hello, this is a go routine")
+}
+
+func main() {
+	go runMe()
+	// ! This is a hacky way of getting the non-main goroutine to complete
+	// Pausing the main goroutine for 1 sec so the other goroutine can complete
+	time.Sleep(1 * time.Second)
+}

c42d3f3c86b7dc5de5c4073c4d037896697df1f8
Kevin Rosengren <rosenkev4@gmail.com>
2025-09-23 14:11:12 -0500

    Added notes on default cases in a select statement

---
+++ concurrency/select/default_select/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	in := make(chan int)
+	out := make(chan int)
+
+	// ? We can use default whenever none of the cases can read or write to thier channels
+	select {
+	case in <- 2:
+		fmt.Println("Wrote 2 to in")
+	case v := <-out:
+		fmt.Println("Wrote", v, "to out")
+	default:
+		fmt.Println("Nothing else worked")
+	}
+}
